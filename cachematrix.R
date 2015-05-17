427d316c6d29cda56e94ebc6437543d1277eb0cd
amcdrng <amcdrng@hotmail.com>
2015-05-17 20:08:26 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix stores a matrix and caches its inverse.
+## cacheSolve returns the inverse of a matrix created with makeCacheMatrix.
 
-## Write a short comment describing this function
 
+# makeCacheMatrix: Stores a matrix and caches its inverse.
+#
+# Returns a list of functions:
+#     set() - set the value of a matrix and set its inverse to NULL
+#     get() - get the matrix
+#     setinv() - set (cache) the inverse of the matrix (not calculated!)
+#     getinv() - get the inverse of the matrix
+#
+# Warning: Does not check whether the matrix is square or singular.
+#          setinv() should only be called from cacheSolve(), as error checking
+#          takes place there.
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+  
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        get <- function() x
+        
+        setinv <- function(inverse) inv <<- inverse
+        
+        getinv <- function() inv
+        
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+# cacheSolve: Returns the inverse of matrix 'x' created with makeCacheMatrix.
+#
+# First checks whether the matrix inverse of x has been cached.
+# If the inverse has been cached, the cached value is returned. Otherwise
+# the inverse is caclculated and cached.
+#
+# Errors: The validity of the matrix is not checked, but if it isn't
+#         square or singular, solve() will throw an error.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        # If the matrix inverse of 'x' is cached, return cached value
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        
+        # Calculate the inverse of 'x', cache and return it.
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
